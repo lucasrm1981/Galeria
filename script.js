045956089952147bae5ce1112ab942836898ad85
Lucas Rodrigues Monteiro <47841382+lucasrm1981@users.noreply.github.com>
2023-01-17 09:06:01 -0300

    Comentários

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
+	/* Utilização da biblioteca fancynibox */
   $(".fancybox").fancybox({
         openEffect: "none",
         closeEffect: "none"
     });
-    
+    /* Chamada do efeito do zoom através do mouse hover */
     $(".zoom").hover(function(){
 		
 		$(this).addClass('transition');
